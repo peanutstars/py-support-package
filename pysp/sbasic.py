937db33b8a87e91a1b6a6882bcec0fec7276fa0f
peanutstars <peanutstars.lee@gmail.com>
2019-02-12 22:33:28 +0900

    Fixed a mistyping.

--- pysp/sbasic.py
+++ pysp/sbasic.py
@@ -46,7 +46,7 @@ class SSingleton(type):
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(SSingleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 
